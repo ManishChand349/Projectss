c0bacac8f7d1805bb7e6036b5bd1041371fb137a
Manish Chand <manishchandr349@gmail.com>
2023-11-26 21:29:16 +0530

    Adding

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import { InputBox } from './components/index'
 
 function App() {
  
   return (
     <>
    <h1 className='text-3xl bg-orange-100'>Currecny App</h1>
+   <InputBox/>
+   <InputBox/>
+   
     </>
   )
 }
