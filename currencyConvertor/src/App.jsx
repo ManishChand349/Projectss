dcd4f2ade9ae0aa0bc510f0b1be53e81957560de
Manish Chand <manishchandr349@gmail.com>
2023-11-28 21:10:06 +0530

    Adding

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -45,8 +45,16 @@ function App() {
               <div className="relative w-full h-0.5">
                 <button
                   type="button"
-                  className="absolute left-1/2-translate-x-1/2-translate-y-1/2 border-2"
+                  className="absolute left-1/2-translate-x-1/2-translate-y-1/2 border-2 border-white rounded-md bg-blue-500 text-white px-2 py-0.5"
+                  onClick={swap}
                 ></button>
+                 <input
+                  label="To"
+                  amount={amount}
+                  currncyOptions={options}
+                  onCurrencyChange={(currency) => setAmount(amount)}
+                  onSelect={from}
+                />
               </div>
             </form>
           </div>
